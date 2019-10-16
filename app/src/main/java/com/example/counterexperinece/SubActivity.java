9340364a3cfff4f2e0fcde28043c9105b21ad065
datsu <atsu34412@gmail.com>
2019-10-16 18:31:29 +0900

    clac function

--- app/src/main/java/com/example/counterexperinece/SubActivity.java
+++ app/src/main/java/com/example/counterexperinece/SubActivity.java
@@ -7,17 +7,21 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
-
+    public int time=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-        setTitle("画面１");
+
+        TextView textView = findViewById(R.id.text_view);
 
         Button btn = (Button)findViewById(R.id.btn);
+        Button btn2 = (Button)findViewById(R.id.btn2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,12 +31,35 @@ public class SubActivity extends AppCompatActivity {
             }
 
         });
+        btnClac.setOnClickListener(new View.OnClickListener() {
+            //クリックイベント
+            @Override
+            public void onClick(View v) {
+                //要素追加
+                calcNum();
+            }
+        });
 
         Intent intent = getIntent();
         int position  = intent.getIntExtra("key",0);
         String name = intent.getStringExtra("name");
-        Toast toast = Toast.makeText(this, String.format(name+" ：%d", position), Toast.LENGTH_LONG);
+        name = name + ": ";
+        textView.setText(name);
+//        Toast toast = Toast.makeText(this, String.format(name+" ：%d", position), Toast.LENGTH_LONG);
+//        toast.setGravity(Gravity.TOP, 0, 150);
+//        toast.show();
+    }
+
+    public void calcNum(){
+        EditText editText1 = findViewById(R.id.edit_text);
+        String str1 = editText1.getText().toString();
+        int num1 = Integer.parseInt(str1);
+        time = num1 + time;
+        Toast toast = Toast.makeText(this, String.format("%d", time), Toast.LENGTH_LONG);
         toast.setGravity(Gravity.TOP, 0, 150);
         toast.show();
+        TextView textView1 = findViewById(R.id.text_view_clac);
+        String str3 = String.valueOf(time);
+        textView1.setText(str3);
     }
 }
